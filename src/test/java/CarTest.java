16970f1b01a18769a4c788aab8d1890b06005afe
Fraser <65736287+fj5585771@users.noreply.github.com>
2021-02-02 19:39:04 +0000

    CarTest.java
    soft wrap implemented

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -9,7 +9,12 @@ public class CarTest {
 //        Given we have a car
         Car car = new Car("SAAB", 2);
 //        When we pas it to the convertor
-       String convertedMessage1 = Converter.process(car);  // 'Converter' is now a second Type of car object.  This class contains a method 'process()', which has a parameter that takes in our 'convert interface' specified method/action of 'convert'.  This is implemented in our Car object, and automatically signals of the method needed to override sourced library capabilities (Converter.process() library method, insisting it takes the parent Vehicle class returned method value instead of sub Car class value that we importantly need).  When testing we have the correct return value from the sub class method, the Converter class method arguement (discussed above^^) contains the convert interface string action method that can be utilised by MORE than one class.
+       String convertedMessage1 = Converter.process(car);  
+        
+//         'Converter' is now a second Type of car object.  
+//         This class contains a method 'process()', which has a parameter that takes in our 'convert interface' specified method/action of 'convert'.  
+//         This is implemented in our Car object, and automatically signals of the method needed to override sourced library capabilities (Converter.process()   library method, insisting it takes the parent Vehicle class returned method value instead of sub Car class value that we importantly need).  
+//             When testing we have the correct return value from the sub class method, the Converter class method arguement (discussed above^^) contains the convert interface string action method that can be utilised by MORE than one class.
 
 //        Then we should see some converted string
         assertEquals("Car data", convertedMessage1);
